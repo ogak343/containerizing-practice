602d74ef8966bef62fda2b725d2079d74e6fed87
khamd <khamdamov343@gmail.com>
2024-04-18 21:58:26 +0500

    Initial commit

---
+++ src/main/java/com/example/containerizingpractice/ContainerizingPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.containerizingpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContainerizingPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContainerizingPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/containerizingpractice/ContainerizingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.containerizingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerizingPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
